67857c9c293500e4e0b2c730d13adfa4b2045ed7
cui1997 <34291675+cui1997@users.noreply.github.com>
2019-05-31 10:38:44 +0800

    Add files via upload

---
+++ baseCom2.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<view>
+			<!-- 滑块 -->
+			<swiper :autoplay="true" :interval='2000' :circular='true'>
+				<swiper-item>
+					<view style="background: #FF7F50;height: 100upx;line-height: 100upx;">A</view>
+				</swiper-item>
+				<swiper-item>
+					<view style="background: red;height: 100upx;line-height: 100upx;">B</view>
+				</swiper-item>
+				<swiper-item>
+					<view style="background: green;height: 100upx;line-height: 100upx;">C</view>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {
+			
+			
+		},
+		mounted() {
+			
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		margin-top: 50upx;
+		text-align: center;
+		height: 400upx;
+	}
+
+	.title {
+		font-size: 36upx;
+	}
+</style>
